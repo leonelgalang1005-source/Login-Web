72bb8ae3623a3da1b475e5a94c69e5c1c029eb77
leonelgalang1005-source <leonelgalang1005@gmail.com>
2025-12-03 15:43:28 +0800

    Create LoginServlet.java

---
+++ LoginServlet.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String user = request.getParameter("username");
+        String pass = request.getParameter("password");
+
+        if (user.equals("admin") && pass.equals("1234")) {
+            response.getWriter().println("Login Successful!");
+        } else {
+            response.getWriter().println("Invalid username or password.");
+        }
+    }
+}
