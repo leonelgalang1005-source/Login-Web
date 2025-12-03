79ff353c482b989ab7cdd78d2e9575428d064c53
leonelgalang1005-source <leonelgalang1005@gmail.com>
2025-12-03 15:44:58 +0800

    Create ForgotPasswordServlet.java

---
+++ ForgotPasswordServlet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/forgot")
+public class ForgotPasswordServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        String email = request.getParameter("email");
+
+        // Demo: just print; later can send real email
+        response.getWriter().println("Password reset link sent to: " + email);
+    }
+}
